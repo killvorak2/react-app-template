152c4fc33c0468633f875b654a28ffe5b39f1a1f
Marcelo <marcelo.ceribelli@gmail.com>
2021-08-29 14:23:26 -0300

    adcionado e testado a estrutura do menu no admin com rotas

---
+++ react/adminDesempenho.tsx
@@ -0,0 +1,8 @@
+// react/adminExample.tsx
+import React, { FC } from 'react'
+
+const AdminDesempenho: FC = () => {
+  return <h1>Hello, World2!</h1>
+}
+
+export default AdminDesempenho
---
+++ react/adminLeads.tsx
@@ -0,0 +1,8 @@
+// react/adminExample.tsx
+import React, { FC } from 'react'
+
+const AdminLeads: FC = () => {
+  return <h1>Hello, World1!</h1>
+}
+
+export default AdminLeads
